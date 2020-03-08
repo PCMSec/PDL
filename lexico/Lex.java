c83492aa3ac69f9fe3556149bb1d23f2e3733eb4
Pablo Castillo <pablo.castillo.martinez@alumnos.upm.es>
2020-03-08 21:50:36 +0100

    Regex por encima

--- lexico/Lex.java
+++ lexico/Lex.java
@@ -11,80 +11,23 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
-
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import token.TiposToken;
 import token.Token;
 
 
 public class Lex {
-	Charset encoding = Charset.defaultCharset();
-	private InputStream in;
-	private Reader r;
-	private String filename;
-
-
-	public Lex(String filename) throws FileNotFoundException {
-		this.filename=filename;
-		this.in=new FileInputStream(filename);
-		this.r=new InputStreamReader(in, encoding);
-
-	}
-
-	public void transito() throws IOException {
-		int estado=0;
-		String lexema=null;
-		int valor=0;
-		int i;
-
-		while((i=r.read()) != -1) {
-			
-			char caracter=leer(i);
-			System.out.print(caracter);
-			switch(estado) {
-			case 0:
-				if (Character.isWhitespace(caracter)) {
-					estado=0;
-				}
-				else if (Character.isLetter(caracter)) {
-					lexema+=caracter;
-					estado=1;
-				}
-				else if (Character.isDigit(caracter)) {
-					valor=Character.getNumericValue(caracter);
-					estado=3;
-				}
-				else if (caracter=='/') {
-					lexema="/";
-					estado=6;
-				}
-				else if(caracter=='-') {
-					lexema="-";
-					estado=10;
-				}
-				else if(caracter=='!') {
-					lexema="!";
-					estado=13;
-				}
-				else if(caracter=='=') {
-					System.out.println("ojoooooooooooo");
-					lexema="=";
-					Token igual=new Token(TiposToken.T_IGUAL);
-					System.out.println(igual.tokenizar());
-					estado=0;
-				}
-				else if(caracter=='"') {
-					lexema="\"";
-					estado=17;
-				}
-				break;
-				//fin de switch
-			}
+	
+	public static String leerArchivo(String filePath) {
+		String resultado="";
+		try {
+			resultado= new String(Files.readAllBytes(Paths.get(filePath)));
 		}
-
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		return resultado;
 	}
-
-	public char leer(int i) {
-		char ch=(char) i;
-		return ch;
-	}
+	
 }
--- principal/Principal.java
+++ principal/Principal.java
@@ -8,18 +8,37 @@ import lexico.Lex;
 public class Principal {
 
 	public static void main(String[] args) {
-		System.out.println("Working Directory = " +
-	              System.getProperty("user.dir"));
-		Lex lexico;
-		try {
-			lexico = new Lex("/home/pablo/eclipse-workspace/PDL/docs/test.txt");
-			lexico.transito();
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		String texto=lexico.Lex.leerArchivo("/home/pablo/eclipse-workspace/PDL/docs/test.txt");
+		//Se deja preparado el texto con el que se va a trabajar
+		texto=texto.replace(";", " ; ");
+		texto=texto.replace(",", " , ");
+		texto=texto.replace(":", " : ");
+		texto=texto.replace("(", " ( ");
+		texto=texto.replace(")", " ) ");
+		texto=texto.replace("[", " [ ");
+		texto=texto.replace("]", " ] ");
+		texto=texto.replace("{", " { ");
+		texto=texto.replace("}", " } ");
+		texto=texto.replace("--", " -- ");
+		texto=texto.replace("`", " ` ");
+		texto=texto.replace("\"", " \" ");
+		texto=texto.replace("=", " = ");
+		texto=texto.replace("!", " ! ");
+		texto=texto.replace("<", " < ");
+		texto=texto.replace(">", " > ");
+		
+		//Se preparan los regex para identificar con lo que se trabaja
+		String identificador=new String("^[a-zA-ZÀ-ÿ]+[0-9|_|a-zA-ZÀ-ÿ]*$");
+		String entero=new String("^\\d+$");
+
+		String tokens[]=texto.split("\\s+");
+		for (String token:tokens) {
+			if (token.matches(entero)) {
+				System.out.println(token + " TRUE");
+			}
+			else {
+				System.out.println(token + " FALSO");
+			}
 		}
 		
 	}
